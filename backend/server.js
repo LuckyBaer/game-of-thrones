58f2f4d79da9252c54927789a04b0994aba843d1
matthias <matze.haegele@gmail.com>
2021-11-06 23:04:39 +0100

    feat(backend): extend api call to make variable requests

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const unirest = require("unirest");
 const port = 3001;
-const baseUrl = "http://www.anapioficeandfire.com/api/houses";
+const baseUrl = "http://www.anapioficeandfire.com/api";
 
-app.get("/api/houses", (req, res) => {
-  const request = unirest("GET", baseUrl);
+app.get("/*", (req, res) => {
+  const request = unirest("GET", baseUrl + req.url);
 
   request.end(function (response) {
     if (response.error) throw new Error(response.error);
